76ca75b1eccabd62476a432677335d3f4ec9aec5
kanazawa midori <imakokoko39@gmail.com>
2024-11-01 19:59:10 +0900

    README修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -103,7 +103,7 @@ Route::get('/test-email', function () {
 
     Mail::raw('This is a test email', function ($message) {
         //to('')に送信先のメールアドレスを入力：例('xxx@example.com')
-        $message->to('')
+        $message->to('test@example')
             ->subject('Test Email');
     });
 
